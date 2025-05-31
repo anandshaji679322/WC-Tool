43861b5eb8281e85bfd1b927fb6e28412300a996
Anand c <anandshaji679322@gmail.com>
2025-05-31 17:15:42 +0530

    Step 2: Implement -l option (line count)

--- ccwc.java
+++ ccwc.java
@@ -1,21 +1,37 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
 
 public class ccwc {
     public static void main(String[] args) {
-        if("-c".equals(args[0])){
-            String fileName = args[1];
-            try(FileInputStream file = new FileInputStream(fileName)){
-                int count=0;
-                while (file.read() != -1){
-                    count++;
+        String fileName = args[1];
+        switch (args[0]){
+            case "-c":
+                try(FileInputStream file = new FileInputStream(fileName)){
+                    int count=0;
+                    while (file.read() != -1){
+                        count++;
+                    }
+                    System.out.println(count +" "+ fileName);
+                }catch (IOException e){
+                    System.out.println(e.getMessage());
                 }
-                System.out.println(count +" "+ fileName);
-            }catch (IOException e){
-                System.out.println(e.getMessage());
-            }
+                break;
+            case  "-l":
+                int count = 0;
+                try {
+                    Scanner sc = new Scanner(new File(fileName));
+                    while (sc.hasNextLine()){
+                        count++;
+                        sc.nextLine();
+                    }
+                }catch (FileNotFoundException e){
+                    System.out.println(e.getMessage());
+                }
+                System.out.println(count + " " + fileName);
+                break;
         }
     }
 }
-
-
