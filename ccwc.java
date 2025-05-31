2998380d9b2b41f73d95997c7a3c667dacb119f1
Anand c <anandshaji679322@gmail.com>
2025-05-31 19:04:01 +0530

    Step 3: Implement -w option (word count)

--- ccwc.java
+++ ccwc.java
@@ -7,22 +7,19 @@ import java.io.File;
 public class ccwc {
     public static void main(String[] args) {
         String fileName = args[1];
+        int count = 0;
         switch (args[0]){
             case "-c":
                 try(FileInputStream file = new FileInputStream(fileName)){
-                    int count=0;
                     while (file.read() != -1){
                         count++;
                     }
-                    System.out.println(count +" "+ fileName);
                 }catch (IOException e){
                     System.out.println(e.getMessage());
                 }
                 break;
             case  "-l":
-                int count = 0;
-                try {
-                    Scanner sc = new Scanner(new File(fileName));
+                try(Scanner sc = new Scanner(new File(fileName))){
                     while (sc.hasNextLine()){
                         count++;
                         sc.nextLine();
@@ -30,8 +27,18 @@ public class ccwc {
                 }catch (FileNotFoundException e){
                     System.out.println(e.getMessage());
                 }
-                System.out.println(count + " " + fileName);
                 break;
+            case "-w":
+                try(Scanner sc = new Scanner(new File(fileName))){
+                    while (sc.hasNext()){
+                        count++;
+                        sc.next();
+                    }
+                }catch (FileNotFoundException e){
+                    System.out.println(e.getMessage());
+                }
+
         }
+        System.out.println(count + " " + fileName);
     }
 }
