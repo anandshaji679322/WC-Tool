6ab62951760b8a8c901d8fc72361bf41929f5206
Anand c <anandshaji679322@gmail.com>
2025-05-31 23:39:38 +0530

    Implemented -w and -l for stdin; file counting works for all flags; -c and -m for stdin still not working, fix postponed

--- ccwc.java
+++ ccwc.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class ccwc {
-    static int findByteCount(String fileName) throws IOException {
+    static int findByteCountFile(String fileName) throws IOException {
         int count = 0;
         try (FileInputStream file = new FileInputStream(fileName)) {
             while (file.read() != -1) {
@@ -11,7 +11,7 @@ public class ccwc {
         }
         return count;
     }
-    static  int findLineCount(String fileName) throws  FileNotFoundException{
+    static  int findLineCountFile(String fileName) throws  FileNotFoundException{
         int count =0;
         try(Scanner sc = new Scanner(new File(fileName))){
             while (sc.hasNextLine()){
@@ -21,7 +21,7 @@ public class ccwc {
         }
         return count;
     }
-    static  int wordCount(String fileName)throws  FileNotFoundException {
+    static  int wordCountFile(String fileName)throws  FileNotFoundException {
         int count = 0;
         try (Scanner sc = new Scanner(new File(fileName))) {
             while (sc.hasNext()) {
@@ -32,7 +32,7 @@ public class ccwc {
         }
         return count;
     }
-    static int findCharacterCount(String fileName)throws IOException{
+    static int findCharacterCountFile(String fileName)throws IOException{
         int count=0;
         try(FileReader fl = new FileReader(fileName)){
             while (fl.read()!=-1){
@@ -41,26 +41,79 @@ public class ccwc {
         }
         return count;
     }
+
+    static int findByteCount() throws IOException {
+        int count = 0;
+            while (System.in.read() != -1) {
+                count++;
+            }
+        return count;
+    }
+    static  int findLineCount() throws  FileNotFoundException{
+        int count =0;
+        try(Scanner sc = new Scanner(System.in)){
+            while (sc.hasNextLine()){
+                count++;
+                sc.nextLine();
+            }
+        }
+        return count;
+    }
+    static  int wordCount()throws  FileNotFoundException {
+        int count = 0;
+        try (Scanner sc = new Scanner(System.in)) {
+            while (sc.hasNext()) {
+                count++;
+                sc.next();
+            }
+
+        }
+        return count;
+    }
+    static int findCharacterCount(Reader input)throws IOException{
+        int count=0;
+            while (input.read()!=-1){
+                count++;
+            }
+        return count;
+    }
+
     public static void main(String[] args) throws IOException {
 
         try{
             if(args.length == 1){
-                System.out.println(findByteCount(args[0]) + " " + findLineCount(args[0]) + " " + wordCount(args[0]) + " " + findCharacterCount(args[0]) + " "+args[0]);
+                switch (args[0]){
+                    case "-c":
+                        System.out.println("stdin"+" "+ findByteCount());
+                        break;
+                    case  "-l":
+                        System.out.println("stdin"+" "+ findLineCount());
+                        break;
+                    case "-w":
+                        System.out.println("stdin"+" "+ wordCount());
+                        break;
+                    case "-m":
+                        System.out.println("stdin"+" "+ findCharacterCount(new InputStreamReader(System.in)));
+                        break;
+                    default:
+                        System.out.println("Invalid Flag -c, -w, -l, or -m");
+
+                }
             }else if(args.length == 2){
                 String fileName = args[1];
                 int count = 0;
                 switch (args[0]){
                     case "-c":
-                        System.out.println(fileName+" "+ findByteCount(fileName));
+                        System.out.println(fileName+" "+ findByteCountFile(fileName));
                         break;
                     case  "-l":
-                        System.out.println(fileName+" "+ findLineCount(fileName));
+                        System.out.println(fileName+" "+ findLineCountFile(fileName));
                         break;
                     case "-w":
-                        System.out.println(fileName+" "+ wordCount(fileName));
+                        System.out.println(fileName+" "+ wordCountFile(fileName));
                         break;
                     case "-m":
-                        System.out.println(fileName+" "+ findCharacterCount(fileName));
+                        System.out.println(fileName+" "+ findCharacterCountFile(fileName));
                         break;
                     default:
                         System.out.println("Invalid Flag -c, -w, -l, or -m");
