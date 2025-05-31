59bb70bb32c82e120310646387bffa8a16bfc3a3
Anand c <anandshaji679322@gmail.com>
2025-05-31 22:19:49 +0530

    Step 4: Refactor code into methods and support default multi-metric output

--- ccwc.java
+++ ccwc.java
@@ -1,44 +1,72 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
-import java.io.File;
 
 public class ccwc {
-    public static void main(String[] args) {
-        String fileName = args[1];
+    static int findByteCount(String fileName) throws IOException {
         int count = 0;
-        switch (args[0]){
-            case "-c":
-                try(FileInputStream file = new FileInputStream(fileName)){
-                    while (file.read() != -1){
-                        count++;
-                    }
-                }catch (IOException e){
-                    System.out.println(e.getMessage());
-                }
-                break;
-            case  "-l":
-                try(Scanner sc = new Scanner(new File(fileName))){
-                    while (sc.hasNextLine()){
-                        count++;
-                        sc.nextLine();
-                    }
-                }catch (FileNotFoundException e){
-                    System.out.println(e.getMessage());
-                }
-                break;
-            case "-w":
-                try(Scanner sc = new Scanner(new File(fileName))){
-                    while (sc.hasNext()){
-                        count++;
-                        sc.next();
-                    }
-                }catch (FileNotFoundException e){
-                    System.out.println(e.getMessage());
-                }
+        try (FileInputStream file = new FileInputStream(fileName)) {
+            while (file.read() != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+    static  int findLineCount(String fileName) throws  FileNotFoundException{
+        int count =0;
+        try(Scanner sc = new Scanner(new File(fileName))){
+            while (sc.hasNextLine()){
+                count++;
+                sc.nextLine();
+            }
+        }
+        return count;
+    }
+    static  int wordCount(String fileName)throws  FileNotFoundException {
+        int count = 0;
+        try (Scanner sc = new Scanner(new File(fileName))) {
+            while (sc.hasNext()) {
+                count++;
+                sc.next();
+            }
 
         }
-        System.out.println(count + " " + fileName);
+        return count;
+    }
+    static int findCharacterCount(String fileName)throws IOException{
+        int count=0;
+        try(FileReader fl = new FileReader(fileName)){
+            while (fl.read()!=-1){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) throws IOException {
+
+        if(args.length == 1){
+            System.out.println(findByteCount(args[0]) + " " + findLineCount(args[0]) + " " + wordCount(args[0]) + " " + findCharacterCount(args[0]) + " "+args[0]);
+        }else if(args.length == 2){
+            String fileName = args[1];
+            int count = 0;
+            switch (args[0]){
+                case "-c":
+                    System.out.println(fileName+" "+ findByteCount(fileName));
+                    break;
+                case  "-l":
+                    System.out.println(fileName+" "+ findLineCount(fileName));
+                    break;
+                case "-w":
+                    System.out.println(fileName+" "+ wordCount(fileName));
+                    break;
+                case "-m":
+                    System.out.println(fileName+" "+ findCharacterCount(fileName));
+                    break;
+                default:
+                    System.out.println("Invalid Flag -c, -w, -l, or -m");
+
+            }
+        }else {
+            System.out.println("Usage: java ccwc [option] filename");
+        }
     }
 }
